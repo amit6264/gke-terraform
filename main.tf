5246ff98a1dde2b8225f9c702eab981ea4730381
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 15:06:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,28 @@ module "apis" {
 
   project_id = var.project_id
 }
+
+
+
+module "network" {
+
+  depends_on = [
+    module.apis
+  ]
+
+  source = "./modules/network"
+
+  project_id = var.project_id
+
+  region = var.region
+
+  network_name = var.network_name
+
+  subnet_name = var.subnet_name
+
+  subnet_cidr = var.subnet_cidr
+
+  pods_cidr = var.pods_cidr
+
+  services_cidr = var.services_cidr
+}
