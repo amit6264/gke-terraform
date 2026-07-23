f3aa573899cb74212c318deaee5cc654ca907b50
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 16:45:33 +0530

    Update main.tf

--- modules/nodepool/main.tf
+++ modules/nodepool/main.tf
@@ -1,10 +1,23 @@
+#############################################################
+# GKE Node Pool
+#############################################################
+
 resource "google_container_node_pool" "primary" {
 
   name     = var.node_pool_name
+  project  = var.project_id
   location = var.region
   cluster  = var.cluster_name
 
+  ###########################################################
+  # Initial Nodes
+  ###########################################################
+
   initial_node_count = var.min_node_count
+
+  ###########################################################
+  # Autoscaling
+  ###########################################################
 
   autoscaling {
 
@@ -13,6 +26,10 @@ resource "google_container_node_pool" "primary" {
 
   }
 
+  ###########################################################
+  # Node Management
+  ###########################################################
+
   management {
 
     auto_repair  = true
@@ -20,49 +37,127 @@ resource "google_container_node_pool" "primary" {
 
   }
 
+  ###########################################################
+  # Upgrade Strategy
+  ###########################################################
+
   upgrade_settings {
 
-    max_surge       = 1
-    max_unavailable = 0
+    max_surge       = var.max_surge
+    max_unavailable = var.max_unavailable
 
   }
 
+  ###########################################################
+  # Node Configuration
+  ###########################################################
+
   node_config {
+
+    #########################################################
+    # Compute
+    #########################################################
 
     machine_type = var.machine_type
 
-    disk_size_gb = var.disk_size_gb
+    spot = var.spot
+
+    #########################################################
+    # Disk
+    #########################################################
 
     disk_type = var.disk_type
 
+    disk_size_gb = var.disk_size_gb
+
+    image_type = var.image_type
+
+    #########################################################
+    # Service Account
+    #########################################################
+
     service_account = var.service_account
 
-    image_type = "COS_CONTAINERD"
+    oauth_scopes = var.oauth_scopes
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    #########################################################
+    # Metadata
+    #########################################################
 
-    labels = {
-      environment = "dev"
-      managed_by  = "terraform"
-    }
+    metadata = {
 
-    tags = [
-      "gke-node"
-    ]
-
-    shielded_instance_config {
-
-      enable_secure_boot          = true
-      enable_integrity_monitoring = true
+      disable-legacy-endpoints = "true"
 
     }
+
+    #########################################################
+    # Workload Metadata
+    #########################################################
 
     workload_metadata_config {
 
       mode = "GKE_METADATA"
 
     }
+
+    #########################################################
+    # Shielded VM
+    #########################################################
+
+    shielded_instance_config {
+
+      enable_secure_boot = true
+
+      enable_integrity_monitoring = true
+
+    }
+
+    #########################################################
+    # Labels
+    #########################################################
+
+    labels = local.common_labels
+
+    #########################################################
+    # Tags
+    #########################################################
+
+    tags = var.node_tags
+
+    #########################################################
+    # Linux Node Config
+    #########################################################
+
+    linux_node_config {
+
+      sysctls = {
+
+        "net.core.somaxconn" = "1024"
+
+      }
+
+    }
+
+    #########################################################
+    # Dynamic Node Taints
+    #########################################################
+
+    dynamic "taint" {
+
+      for_each = var.node_taints
+
+      content {
+
+        key = taint.value.key
+
+        value = taint.value.value
+
+        effect = taint.value.effect
+
+      }
+
+    }
+
   }
+
 }
