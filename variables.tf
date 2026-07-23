cc4d75e13f2dc2a454102c0c1a8fd735c41585ca
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 15:27:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,3 +30,30 @@ variable "repository_name" {
 variable "cluster_name" {
   type = string
 }
+
+
+
+
+variable "node_pool_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "disk_size_gb" {
+  type = number
+}
+
+variable "disk_type" {
+  type = string
+}
+
+variable "min_node_count" {
+  type = number
+}
+
+variable "max_node_count" {
+  type = number
+}
