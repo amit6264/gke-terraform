1d855e17ad2a9470d87808fe5efbdad1d9945360
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 16:01:04 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -13,3 +13,11 @@ output "subnet_name" {
 output "subnet_id" {
   value = google_compute_subnetwork.private.id
 }
+
+output "pods_secondary_range_name" {
+  value = google_compute_subnetwork.private.secondary_ip_range[0].range_name
+}
+
+output "services_secondary_range_name" {
+  value = google_compute_subnetwork.private.secondary_ip_range[1].range_name
+}
