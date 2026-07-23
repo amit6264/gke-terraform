0a227a69f3280c8f7dda6c666655420d9ef9ae49
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 16:22:10 +0530

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,11 +1,14 @@
-output "gke_node_sa_email" {
-  value = google_service_account.gke_node_sa.email
+output "gke_node_service_account_email" {
+
+  value = google_service_account.gke_node.email
 }
 
-output "github_actions_sa_email" {
-  value = google_service_account.github_actions_sa.email
+output "github_actions_service_account_email" {
+
+  value = google_service_account.github.email
 }
 
-output "argocd_sa_email" {
-  value = google_service_account.argocd_sa.email
+output "argocd_service_account_email" {
+
+  value = google_service_account.argocd.email
 }
