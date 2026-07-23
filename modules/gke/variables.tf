942c8247ac829087a05020fd31364814062cddeb
Amit Patidar <amit.patidar6264@gmail.com>
2026-07-23 16:26:55 +0530

    Update variables.tf

--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -1,23 +1,94 @@
 variable "project_id" {
-  type = string
+  description = "Google Cloud Project ID"
+  type        = string
 }
 
 variable "region" {
-  type = string
+  description = "Cluster Region"
+  type        = string
 }
 
 variable "cluster_name" {
-  type = string
+  description = "GKE Cluster Name"
+  type        = string
 }
 
 variable "network" {
-  type = string
+  description = "VPC Name"
+  type        = string
 }
 
 variable "subnetwork" {
-  type = string
+  description = "Subnet Name"
+  type        = string
 }
 
-variable "gke_service_account" {
+variable "pods_secondary_range_name" {
+  description = "Pods Secondary Range"
+  type        = string
+}
+
+variable "services_secondary_range_name" {
+  description = "Services Secondary Range"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
+variable "master_ipv4_cidr_block" {
+  description = "Master CIDR Block"
+  type        = string
+
+  default = "172.16.0.0/28"
+}
+
+variable "enable_private_nodes" {
+
+  type = bool
+
+  default = true
+
+}
+
+variable "enable_private_endpoint" {
+
+  type = bool
+
+  default = false
+
+}
+
+variable "release_channel" {
+
   type = string
+
+  default = "REGULAR"
+
+}
+
+variable "admin_cidr" {
+
+  type = string
+
+  description = "Office VPN/Public IP"
+
+}
+
+variable "deletion_protection" {
+
+  type = bool
+
+  default = false
+
+}
+
+variable "maintenance_start_time" {
+
+  type = string
+
+  default = "03:00"
+
 }
